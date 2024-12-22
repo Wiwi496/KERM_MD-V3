62c74b9a2312f398cb42385012603a0c5664de2d
Wiwi496 <koffiromeoaristide@gmail.com>
2024-12-22 01:47:26 +0000

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -20,7 +20,7 @@ const config = {
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
   OWNER_NAME: process.env.OWNER_NAME || "☞⌜ KG TECH⌝☜",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "237659535227",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "22250150188641",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
